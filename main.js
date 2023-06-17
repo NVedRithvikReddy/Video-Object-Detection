32e7ec6925a85b317a3535973f43e89b966fe23c
NVedRithvikReddy <129588629+NVedRithvikReddy@users.noreply.github.com>
2023-06-17 14:32:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,24 @@
-img = "";
+video = "";
 status = "";
-objects = [];
-function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380, 380);
+function preload() {
+    video = createVideo("video.mp4");
     video.hide();
+}
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
 }
 function start() {
     objectDetector = ml5.objectDetector("cocossd", modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting object";
+    document.getElementById("status").innerHTML = "Status : started detecting objects";
 }
 function modelLoaded() {
     console.log("modelLoaded");
     status = true;
-}
-function gotResult(error, results) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(results);
-        objects = results;
-    }
-}
-
-function preload() {
-    img = loadImage("dog_cat.jpg");
-}
-
-function draw() {
-    image(video, 0, 0, 380, 380);
-    if (status != "") {
-        r = random(255);
-        g = random(255);
-        b = random(255);
-        objectDetector.detect(video, gotResult);
-        for (i = 0; i < objects.length; i++) {
-            document.getElementById("status").innerHTML = "Status : Object Detected";
-            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length;
-            fill(r, g, b);
-            percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 30);
-            noFill();
-            stroke(r, g, b);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-    }
+    video.loop()
+    video.volume(0);
+    video.speed(1);
 }
